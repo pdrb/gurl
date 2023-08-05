8cef98ab9ad3195789267ab1c0899d45399d8abe
Pedro Gonring <pedro@bigode.net>
2023-08-05 18:51:54 -0300

    Update build

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ linux_amd64_file='gurl-linux-x86_64'
 linux_arm64_file='gurl-linux-arm64'
 mac_amd64_file='gurl-mac-x86_64'
 mac_arm64_file='gurl-mac-arm64'
+windows_amd64_file='gurl-windows-x86_64.exe'
+windows_arm64_file='gurl-windows-arm64.exe'
 
 # Show go version
 go version
@@ -17,21 +19,31 @@ echo
 # Linux
 echo "------- Linux Build -------"
 echo "- Building for amd64..."
-GOOS=linux GOARCH=amd64 go build -o "$linux_amd64_file" main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "$linux_amd64_file"
 gzip -f "$linux_amd64_file"
 echo "- Building for arm64..."
-GOOS=linux GOARCH=arm64 go build -o "$linux_arm64_file" main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o "$linux_arm64_file"
 gzip -f "$linux_arm64_file"
 echo
 
 # Mac
 echo "------- Mac Build -------"
 echo "- Building for amd64..."
-GOOS=darwin GOARCH=amd64 go build -o "$mac_amd64_file" main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o "$mac_amd64_file"
 gzip -f "$mac_amd64_file"
 echo "- Building for arm64..."
-GOOS=darwin GOARCH=arm64 go build -o "$mac_arm64_file" main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o "$mac_arm64_file"
 gzip -f "$mac_arm64_file"
+echo
+
+# Windows
+echo "------- Windows Build -------"
+echo "- Building for amd64..."
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o "$windows_amd64_file"
+gzip -f "$windows_amd64_file"
+echo "- Building for arm64..."
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -o "$windows_arm64_file"
+gzip -f "$windows_arm64_file"
 echo
 
 echo -e "Build successfully! :)"
