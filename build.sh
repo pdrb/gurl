c61b14623fa0072c13e2065e149911ce9d2b105f
Pedro Gonring <pedro@bigode.net>
2023-08-15 23:23:33 -0300

    Add strip flags

--- build.sh
+++ build.sh
@@ -19,30 +19,30 @@ echo
 # Linux
 echo "------- Linux Build -------"
 echo "- Building for amd64..."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "$linux_amd64_file"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o "$linux_amd64_file"
 gzip -f "$linux_amd64_file"
 echo "- Building for arm64..."
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o "$linux_arm64_file"
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o "$linux_arm64_file"
 gzip -f "$linux_arm64_file"
 echo
 
 # Mac
 echo "------- Mac Build -------"
 echo "- Building for amd64..."
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o "$mac_amd64_file"
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o "$mac_amd64_file"
 gzip -f "$mac_amd64_file"
 echo "- Building for arm64..."
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o "$mac_arm64_file"
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o "$mac_arm64_file"
 gzip -f "$mac_arm64_file"
 echo
 
 # Windows
 echo "------- Windows Build -------"
 echo "- Building for amd64..."
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o "$windows_amd64_file"
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o "$windows_amd64_file"
 gzip -f "$windows_amd64_file"
 echo "- Building for arm64..."
-CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -o "$windows_arm64_file"
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -ldflags="-s -w" -o "$windows_arm64_file"
 gzip -f "$windows_arm64_file"
 echo
 
